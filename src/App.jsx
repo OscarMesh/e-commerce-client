7213ac42247713aa36c3440e5fb3160070bff119
osacrMesh <okerekeifeanyi65@gmail.com>
2022-12-28 22:55:57 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+} from "react-router-dom";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login">{user ? redirect("/") : <Login />}</Route>
+        <Route path="/products:category" element={<ProductList />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
