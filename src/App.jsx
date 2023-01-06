38e63bff907c6f446099e89df67aa8383f686bdf
osacrMesh <okerekeifeanyi65@gmail.com>
2023-01-06 11:01:43 +0100

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,16 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const user = true;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login">{user ? redirect("/") : <Login />}</Route>
+        <Route path="/login" element={<Login />} />
+
         <Route path="/products:category" element={<ProductList />} />
         <Route path="/products/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
